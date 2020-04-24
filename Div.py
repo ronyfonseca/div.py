1a867cd4c91f04c9aa495b500feabf9b7f5f7626
ronyfonseca <54121959+ronyfonseca@users.noreply.github.com>
2020-04-24 17:09:12 -0300

    Create Div.py

---
+++ Div.py
@@ -0,0 +1,46 @@
+a1 = int(input('DIGITE O NÚMERO:'))
+l = ('===')
+b1 = a1 * 1
+b2 = a1 * 2
+b3 = a1 * 3
+b4 = a1 * 4
+b5 = a1 * 5
+b6 = a1 * 6
+b7 = a1 * 7
+b8 = a1 * 8
+b9 = a1 * 9
+b10 = a1 * 10
+print('======================')
+print(' {} x 1 = {}'.format(a1, b1))
+print(' {} x 2 = {}'.format(a1, b2))
+print(' {} x 3 = {}'.format(a1, b3))
+print(' {} x 4 = {}'.format(a1, b4))
+print(' {} x 5 = {}'.format(a1, b5))
+print(' {} x 6 = {}'.format(a1, b6))
+print(' {} x 7 = {}'.format(a1, b7))
+print(' {} x 8 = {}'.format(a1, b8))
+print(' {} x 9 = {}'.format(a1, b9))
+print(' {} x 10 = {}'.format(a1, b10))
+print('====================')
+c1  = a1 / 1
+c2 = a1 / 2
+c3 = a1 / 3
+c4 = a1 / 4
+c5 = a1 / 5
+c6 = a1 / 6
+c7 = a1 / 7
+c8 = a1 / 8
+c9 = a1 / 9
+c10 = a1 / 10
+print('======================')
+print(' {} dividido 1 = {}'.format(a1, c1))
+print(' {} dividido 2 = {}'.format(a1, c2))
+print(' {} dividido 3 = {}'.format(a1, c3))
+print(' {} dividido 4 = {}'.format(a1, c4))
+print(' {} dividido 5 = {}'.format(a1, c5))
+print(' {} dividido 6 = {}'.format(a1, c6))
+print(' {} dividido 7 = {}'.format(a1, c7))
+print(' {} dividido 8 = {}'.format(a1, c8))
+print(' {} dividido 9 = {}'.format(a1, c9))
+print(' {} dividido 10 = {}'.format(a1, c10))
+print('====================')
